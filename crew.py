9d2d17943526233434b8dc44959ded6da0cb51b7
AIWalaBro <aiwalabro@gmail.com>
2024-05-28 08:41:27 +0530

    crew file updated

---
+++ crew.py
@@ -0,0 +1,19 @@
+from crewai import Crew,Process
+from agents import blog_researcher,blog_writer
+from tasks import research_task,write_task
+
+# Forming the tech-focused crew with some enhanced configurations
+crew = Crew(
+    agents=[blog_researcher, blog_writer],
+    tasks=[research_task, write_task],
+    process=Process.sequential, # Optional: Sequential task execution is
+    memory=True,
+    cache=True,
+    max_rpm=100,
+    share_crew=True
+)
+
+# start the task exceution process enhanced feedback
+
+result = crew.kickoff(inputs = {'topic' : 'AI VS ML VS DL VS Data Science'})
+print(result)
